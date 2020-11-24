b9fc97e604620dd44312728701bc4ed91961a47a
Abdu <abdukr5@gmail.com.com>
2020-11-24 12:07:21 -0600

    gender added for each top 76 authors

--- PoKi.sql
+++ PoKi.sql
@@ -7,7 +7,18 @@
 --  GROUP BY e.Id,e.Name
 --3)Select COUNT(Id) AS 'Number of Poems'
 --  FROM Poem 
-SELECT Name
-FROM Author 
-WHERE Id BETWEEN 1 AND 76
-ORDER BY Name
+--4)SELECT Name
+--  FROM Author 
+--  WHERE Id BETWEEN 1 AND 76
+--  ORDER BY Name
+--5)SELECT a.Name, g.Name
+--  FROM Author a
+--  LEFT JOIN Grade g ON g.Id = a.GradeId 
+--  WHERE a.Id BETWEEN 1 AND 76
+--  ORDER BY a.Name
+SELECT a.Name, g.Name, gn.Name
+FROM Author a
+LEFT JOIN Grade g ON g.Id = a.GradeId 
+LEFT JOIN Gender gn ON gn.Id = a.GenderId
+WHERE a.Id BETWEEN 1 AND 76
+ORDER BY a.Name
